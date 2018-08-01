5aa9e74215d6ace07b16b5b1bcee9417e5fc2f35
Rico Picone <ricopicone@gmail.com>
2018-07-31 17:21:17 -0700

    prettified

--- sim_pde.m
+++ sim_pde.m
@@ -1,34 +1,29 @@
 % --------------------------------------------------------------
 function [cm,f,s] = sim_pde(r,t,p,DpDx)
 
-%---Constants and parameters
+% constants and parameters
 global ii g G D c rho_3_langevin T1 mu_p mu_e B0 B_d kB temp T12 T13 freq duty rmax sim T
 
 cm = [1; 1; 1];                                  
-f = [   1+G; ...
-        1; ...
-        G].*DpDx;          
-% p_init = PiconeTwoSpeciesTransport_ic(r);
+f = [...
+    1+G; ...
+    1; ...
+    G ...
+].*DpDx;
 
-%---T1 terms
+% T1 terms
 switch sim
     case 'performed'
         simPulsePerformedExperiment;
     case 'rugar1'
-%         simPulseRugar1;
         T12Term = 0;
         T13Term = 0;
     otherwise
         T12Term = 0;
         T13Term = 0;
 end
-    
-%---verification    
-% s = [   -(1+G*D*g^2*(p(2)^2-p(3)^2))*c^2*p(1)-c/(1+D)*(DpDx(2)-G*D*g*DpDx(3)); ...
-%          ((1-p(2)^2)*DpDx(1)-2*p(1)*p(2)*DpDx(2))*c; ...
-%         -((1-p(3)^2)*DpDx(1)-2*p(1)*p(3)*DpDx(3))*c*G*g];  
 
-%---thesis
+% separation terms
 s= [    -c^2/(1+D)*( (1-p(2)^2) + G*D*g^2*(1-p(3)^2) )*atanh(p(1))-c/(1+D)*(DpDx(2)-G*D*g*DpDx(3)); ...
          c*( (1-p(2)^2)/(1-p(1)^2)*DpDx(1) - 2*atanh(p(1))*p(2)*DpDx(2) ); ...
         -c*G*g*( (1-p(3)^2)/(1-p(1)^2)*DpDx(1) - 2*atanh(p(1))*p(3)*DpDx(3) ) ];  
