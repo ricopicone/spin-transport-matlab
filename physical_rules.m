ecf7ca71ccdb98ba0cf6ab7c1f7005d83eea101f
Rico Picone <ricopicone@gmail.com>
2018-08-21 17:36:34 -0700

    Neumann boundary conditions in equilibrium looks good

--- physical_rules.m
+++ physical_rules.m
@@ -1,7 +1,7 @@
 %---physical rules
-global T ii g G D B_r c T1 rmax u0 T12 T13 mu_p mu_e B0 B_d kB temp freq duty rho_1_langevin rho_2_langevin rho_3_langevin
+global T ii g G D B_r c T1 rmax u0 T12 T13 mu_p mu_e B0 B_d kB temp freq duty rho_1_langevin rho_2_langevin rho_3_langevin  d_rho_1_langevin d_rho_2_langevin d_rho_3_langevin
 
-% Langevin
+% precise constants to be used
 digits(50)
 precise_constant = vpa(...
     -1*... % I think this is because mag grad is negative
@@ -15,6 +15,16 @@ precise_constant = vpa(...
         ) ...
     ) ...
 );
+
+mu_p_kB = vpa(mu_p/kB);
+mu_e_kB = vpa(mu_e/kB);
+
+% Langevin (equilibrium solution)
 rho_1_langevin= @(r) double(precise_constant);    % langevin nuclear polarization
-rho_2_langevin= @(r)  tanh((B0-B_d*r)*mu_p/kB/temp);    % langevin nuclear polarization
-rho_3_langevin= @(r)  tanh((B0-B_d*r)*mu_e/kB/temp);    % langevin electron polarization
+rho_2_langevin= @(r)  tanh((B0-B_d*r)*mu_p_kB/temp);    % langevin nuclear polarization
+rho_3_langevin= @(r)  tanh((B0-B_d*r)*mu_e_kB/temp);    % langevin electron polarization
+
+% gradient of equilibrium solution
+d_rho_1_langevin= @(r) 0;
+d_rho_2_langevin= @(r)  double(-B_d*mu_p_kB/temp*(sech((B0-B_d*r)*mu_p_kB/temp))^2);
+d_rho_3_langevin= @(r)  double(-B_d*mu_e_kB/temp*(sech((B0-B_d*r)*mu_e_kB/temp))^2);
--- sim_bc.m
+++ sim_bc.m
@@ -1,14 +1,12 @@
 % --------------------------------------------------------------
 function [pl,ql,pr,qr] = sim_bc(xl,ul,xr,ur,t)
-global u0 c g G rho_1_langevin rho_2_langevin rho_3_langevin
+global u0 c g G rho_1_langevin rho_2_langevin rho_3_langevin d_rho_1_langevin d_rho_2_langevin d_rho_3_langevin
 
 %---pinned
-% pl = [ul(1); ul(2)-u0(2); ul(3)-u0(3)];
-pl = ul - [rho_1_langevin(xl);rho_2_langevin(xl);rho_3_langevin(xl)];
-ql = [0;0;0];                                  
-% pr = [ur(1); ur(2)-u0(2); ur(3)-u0(3)];  
-pr = ur - [rho_1_langevin(xr);rho_2_langevin(xr);rho_3_langevin(xr)];
-qr = [0;0;0];      
+% pl = ul - [rho_1_langevin(xl);rho_2_langevin(xl);rho_3_langevin(xl)];
+% ql = [0;0;0];
+% pr = ur - [rho_1_langevin(xr);rho_2_langevin(xr);rho_3_langevin(xr)];
+% qr = [0;0;0];      
 
 % %---zero gradient
 % pl = [0; 0; 0];                               
@@ -22,12 +20,10 @@ qr = [0;0;0];
 % pr = [0; -c*(1-ur(2)^2)*atanh(ur(1)); -c*G*g*(1-ur(3)^2)*atanh(ur(1))];                         
 % qr = [-1;-1;-1];     
 
-%---equil j
-% pl = [0; -c*(1-ul(2)^2)*atanh(ul(1)); -c*G*g*(1-ul(3)^2)*atanh(ul(1))];                               
-% ql = [-1;-1;-1];                                  
-% pr = [0; -c*(1-ur(2)^2)*atanh(ur(1)); -c*G*g*(1-ur(3)^2)*atanh(ur(1))];                         
-% qr = [-1;-1;-1];  
-% pl = pl + [rho_1_langevin(xl);rho_2_langevin(xl);rho_3_langevin(xl)];
-% pr = pr + [rho_1_langevin(xr);rho_2_langevin(xr);rho_3_langevin(xr)];
+%---equil j                             
+ql = [-1/(1+G);-1;-1/G];                                                         
+qr = [-1/(1+G);-1;-1/G];  
+pl = [d_rho_1_langevin(xl);d_rho_2_langevin(xl);d_rho_3_langevin(xl)];
+pr = [d_rho_1_langevin(xr);d_rho_2_langevin(xr);d_rho_3_langevin(xr)];
 
 end
