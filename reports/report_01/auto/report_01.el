7487064cde056cfb6686f60506d764313efaeb99
Rico Picone <ricopicone@gmail.com>
2019-05-21 06:56:13 -0700

    looks like latex packages

--- reports/report_01/auto/report_01.el
+++ reports/report_01/auto/report_01.el
@@ -5,12 +5,12 @@
                      '(("amsart" "onecolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("xcolor" "hyperref" "x11names")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -36,19 +36,19 @@
    (TeX-add-symbols
     '("Diff" 1))
    (LaTeX-add-labels
-    "sec:org50cf7cc"
-    "sec:org96530b0"
+    "sec:org54ab8da"
+    "sec:org05792ba"
     "eq:diffusion"
     "eq:solution"
     "eq:solution-dirac"
-    "sec:org70efa3f"
+    "sec:org795705d"
     "fig:1a"
     "fig:1b"
     "fig:1"
     "fig:2a"
     "fig:2b"
     "fig:2"
-    "sec:orgece42eb")
+    "sec:org6d61818")
    (LaTeX-add-bibliographies))
  :latex)
 
