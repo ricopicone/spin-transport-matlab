5f5624935ed8665b027901a59f5eaf9df2b22b90
Rico Picone <ricopicone@gmail.com>
2019-05-22 20:15:41 -0700

    mostly implemented class_definition property to save a copy of the class definition file as a plain text property in each class instance

--- spin_transport_simulation.m
+++ spin_transport_simulation.m
@@ -1,18 +1,19 @@
 classdef spin_transport_simulation
    properties
-      timestamp
+      class_version = 0.1; % float for easy version comparison
+      class_definition % to save a copy of this classdef file for comparison of serialized instances
+      timestamp = datestr(now); % timestamp of class creation
       physical_constants
       pde
-      initial_conditions
-      boundary_conditions
-      ode_solver_options
-      simulation_parameters
+      initial_conditions = {};
+      boundary_conditions = {};
+      ode_solver_options = {};
+      simulation_parameters = {};
+      simulation_results = {};
    end
    methods
-       function obj = spin_transport_simulation()
-          if nargin == 0
-             obj.timestamp = datestr(now);
-          end
+       function obj = spin_transport_simulation() % called at instance creation
+             obj.class_definition = fileread([mfilename(),'.m']);
        end
    end
 end
